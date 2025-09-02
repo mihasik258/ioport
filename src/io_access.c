bdcc8294e1c25c53e85fc5035a793355a34af807
Toaster <81384763+mihasik258@users.noreply.github.com>
2025-09-02 20:06:07 +0300

    linux style + remove duplicates

--- src/io_access.c
+++ src/io_access.c
@@ -8,172 +8,169 @@
 #include <errno.h>
 #include <string.h>
 
-#define PAGE_SIZE 4096
-#define PORT_MASK 0xFFFF
+#define PAGE_SIZE  4096
+#define PORT_MASK  0xFFFF
 
-static int dev_mem_fd = -1;
-static bool has_port_access = false;
+static int  dev_mem_fd = -1;
+static bool  has_port_access = false;
 
 static inline bool is_port_address(uintptr_t addr)
 {
-	return addr <= PORT_MASK;
+  return addr <= PORT_MASK;
 }
 
 static inline uintptr_t align_to_page(uintptr_t addr)
 {
-	return addr & ~(PAGE_SIZE - 1);
+  return addr & ~(PAGE_SIZE - 1);
 }
 
 static inline size_t get_page_offset(uintptr_t addr)
 {
-	return addr & (PAGE_SIZE - 1);
+  return addr & (PAGE_SIZE - 1);
+}
+
+static void *map_addr(uintptr_t addr, int prot)
+{
+  uintptr_t page_base = align_to_page(addr);
+
+  void *map = mmap(NULL, PAGE_SIZE, prot, MAP_SHARED,
+       dev_mem_fd, page_base);
+  if (map == MAP_FAILED) {
+    fprintf(stderr, "Failed to map memory at 0x%lx: %s\n",
+      (unsigned long)addr, strerror(errno));
+    return NULL;
+  }
+
+  return map;
+}
+
+static inline uint64_t mem_read(uintptr_t addr, size_t size)
+{
+  void *map = map_addr(addr, PROT_READ);
+  if (!map)
+    return 0;
+
+  off_t offset = get_page_offset(addr);
+  uint64_t val = 0;
+
+  switch (size) {
+  case 1:
+    val = *((volatile uint8_t *)((uintptr_t)map + offset));
+    break;
+  case 2:
+    val = *((volatile uint16_t *)((uintptr_t)map + offset));
+    break;
+  case 4:
+    val = *((volatile uint32_t *)((uintptr_t)map + offset));
+    break;
+  default:
+    fprintf(stderr, "Unsupported read size %zu\n", size);
+  }
+
+  munmap(map, PAGE_SIZE);
+  return val;
+}
+
+static inline void mem_write(uintptr_t addr, uint64_t value, size_t size)
+{
+  void *map = map_addr(addr, PROT_READ | PROT_WRITE);
+  if (!map)
+    return;
+
+  off_t offset = get_page_offset(addr);
+
+  switch (size) {
+  case 1:
+    *((volatile uint8_t *)((uintptr_t)map + offset)) = (uint8_t)value;
+    break;
+  case 2:
+    *((volatile uint16_t *)((uintptr_t)map + offset)) = (uint16_t)value;
+    break;
+  case 4:
+    *((volatile uint32_t *)((uintptr_t)map + offset)) = (uint32_t)value;
+    break;
+  default:
+    fprintf(stderr, "Unsupported write size %zu\n", size);
+  }
+
+  munmap(map, PAGE_SIZE);
 }
 
 bool io_init(void)
 {
-	if (geteuid() != 0)
-		fprintf(stderr, "Warning: Root privileges required for direct hardware access\n");
+  if (geteuid() != 0)
+    fprintf(stderr, "Warning: Root privileges required for direct hardware access\n");
 
-	if (iopl(3) == 0) {
-		has_port_access = true;
-	} else {
-		fprintf(stderr, "Warning: Failed to get I/O port access: %s\n", strerror(errno));
-		has_port_access = false;
-	}
+  if (iopl(3) == 0) {
+    has_port_access = true;
+  } else {
+    fprintf(stderr, "Warning: Failed to get I/O port access: %s\n",
+      strerror(errno));
+    has_port_access = false;
+  }
 
-	dev_mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
-	if (dev_mem_fd < 0) {
-		fprintf(stderr, "Warning: Failed to open /dev/mem: %s\n", strerror(errno));
-		return has_port_access;
-	}
+  dev_mem_fd = open("/dev/mem", O_RDWR | O_SYNC);
+  if (dev_mem_fd < 0) {
+    fprintf(stderr, "Warning: Failed to open /dev/mem: %s\n",
+      strerror(errno));
+    return has_port_access;
+  }
 
-	return true;
+  return true;
 }
 
 void io_cleanup(void)
 {
-	if (dev_mem_fd >= 0) {
-		close(dev_mem_fd);
-		dev_mem_fd = -1;
-	}
+  if (dev_mem_fd >= 0) {
+    close(dev_mem_fd);
+    dev_mem_fd = -1;
+  }
 }
 
 uint8_t io_read_byte(uintptr_t addr)
 {
-	if (is_port_address(addr) && has_port_access)
-		return inb(addr);
-
-	uintptr_t page_base = align_to_page(addr);
-	off_t offset = get_page_offset(addr);
-
-	void *map = mmap(NULL, PAGE_SIZE, PROT_READ, MAP_SHARED, dev_mem_fd, page_base);
-	if (map == MAP_FAILED) {
-		fprintf(stderr, "Failed to map memory: %s\n", strerror(errno));
-		return 0;
-	}
-
-	uint8_t value = *((volatile uint8_t *)((uintptr_t)map + offset));
-	munmap(map, PAGE_SIZE);
-
-	return value;
+  if (is_port_address(addr) && has_port_access)
+    return inb(addr);
+  return (uint8_t)mem_read(addr, 1);
 }
 
 uint16_t io_read_word(uintptr_t addr)
 {
-	if (is_port_address(addr) && has_port_access)
-		return inw(addr);
-
-	uintptr_t page_base = align_to_page(addr);
-	off_t offset = get_page_offset(addr);
-
-	void *map = mmap(NULL, PAGE_SIZE, PROT_READ, MAP_SHARED, dev_mem_fd, page_base);
-	if (map == MAP_FAILED) {
-		fprintf(stderr, "Failed to map memory: %s\n", strerror(errno));
-		return 0;
-	}
-
-	uint16_t value = *((volatile uint16_t *)((uintptr_t)map + offset));
-	munmap(map, PAGE_SIZE);
-
-	return value;
+  if (is_port_address(addr) && has_port_access)
+    return inw(addr);
+  return (uint16_t)mem_read(addr, 2);
 }
 
 uint32_t io_read_dword(uintptr_t addr)
 {
-	if (is_port_address(addr) && has_port_access)
-		return inl(addr);
-
-	uintptr_t page_base = align_to_page(addr);
-	off_t offset = get_page_offset(addr);
-
-	void *map = mmap(NULL, PAGE_SIZE, PROT_READ, MAP_SHARED, dev_mem_fd, page_base);
-	if (map == MAP_FAILED) {
-		fprintf(stderr, "Failed to map memory: %s\n", strerror(errno));
-		return 0;
-	}
-
-	uint32_t value = *((volatile uint32_t *)((uintptr_t)map + offset));
-	munmap(map, PAGE_SIZE);
-
-	return value;
+  if (is_port_address(addr) && has_port_access)
+    return inl(addr);
+  return (uint32_t)mem_read(addr, 4);
 }
 
 void io_write_byte(uintptr_t addr, uint8_t value)
 {
-	if (is_port_address(addr) && has_port_access) {
-		outb(value, addr);
-		return;
-	}
-
-	uintptr_t page_base = align_to_page(addr);
-	off_t offset = get_page_offset(addr);
-
-	void *map = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, dev_mem_fd, page_base);
-	if (map == MAP_FAILED) {
-		fprintf(stderr, "Failed to map memory: %s\n", strerror(errno));
-		return;
-	}
-
-	*((volatile uint8_t *)((uintptr_t)map + offset)) = value;
-	munmap(map, PAGE_SIZE);
+  if (is_port_address(addr) && has_port_access) {
+    outb(value, addr);
+    return;
+  }
+  mem_write(addr, value, 1);
 }
 
 void io_write_word(uintptr_t addr, uint16_t value)
 {
-	if (is_port_address(addr) && has_port_access) {
-		outw(value, addr);
-		return;
-	}
-
-	uintptr_t page_base = align_to_page(addr);
-	off_t offset = get_page_offset(addr);
-
-	void *map = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, dev_mem_fd, page_base);
-	if (map == MAP_FAILED) {
-		fprintf(stderr, "Failed to map memory: %s\n", strerror(errno));
-		return;
-	}
-
-	*((volatile uint16_t *)((uintptr_t)map + offset)) = value;
-	munmap(map, PAGE_SIZE);
+  if (is_port_address(addr) && has_port_access) {
+    outw(value, addr);
+    return;
+  }
+  mem_write(addr, value, 2);
 }
 
 void io_write_dword(uintptr_t addr, uint32_t value)
 {
-	if (is_port_address(addr) && has_port_access) {
-		outl(value, addr);
-		return;
-	}
-
-	uintptr_t page_base = align_to_page(addr);
-	off_t offset = get_page_offset(addr);
-
-	void *map = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, dev_mem_fd, page_base);
-	if (map == MAP_FAILED) {
-		fprintf(stderr, "Failed to map memory: %s\n", strerror(errno));
-		return;
-	}
-
-	*((volatile uint32_t *)((uintptr_t)map + offset)) = value;
-	munmap(map, PAGE_SIZE);
+  if (is_port_address(addr) && has_port_access) {
+    outl(value, addr);
+    return;
+  }
+  mem_write(addr, value, 4);
 }
