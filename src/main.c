723b139d1607736a3fbe2bdc1955417d6dd6d808
mihasik258 <miha.kuritsin@gmail.com>
2025-09-11 17:11:57 +0300

    reworking command history

--- src/main.c
+++ src/main.c
@@ -10,11 +10,12 @@ l would be enough to make it work, so I didn't
 #include <unistd.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+#include <signal.h>
 #include "io_access.h"
 #include "command_processor.h"
 
 #define MAX_INPUT_LENGTH 1024
-#define HISTORY_SIZE 100
+#define HISTORY_BUFFER_SIZE 4096
 #define MAX_CMD_LENGTH 16
 #define MAX_ARG_LENGTH 32
 
@@ -33,15 +34,92 @@ l would be enough to make it work, so I didn't
 #define NULL_TERMINATOR '\0'
 #define NEWLINE_CHAR '\n'
 
-static char history[HISTORY_SIZE][MAX_INPUT_LENGTH];
-static int history_count = 0;
-static int history_current = 0;
+static struct termios original_termios_global;
+static int termios_initialized = 0;
+
+typedef struct history_entry {
+    char *command;
+    struct history_entry *prev;
+    struct history_entry *next;
+} history_entry_t;
+
+static history_entry_t *history_head = NULL;
+static history_entry_t *history_tail = NULL;
+static history_entry_t *history_current = NULL;
+static size_t history_memory_used = 0;
+static size_t history_count = 0;
+
+static void add_to_history(const char *command) {
+    size_t command_len = strlen(command) + 1;
+    while (history_memory_used + command_len > HISTORY_BUFFER_SIZE && history_head != NULL) {
+        history_entry_t *oldest = history_head;
+        size_t oldest_len = strlen(oldest->command) + 1;
+        history_memory_used -= oldest_len;
+        history_count--;        
+        history_head = oldest->next;
+        if (history_head != NULL) {
+            history_head->prev = NULL;
+        } else {
+            history_tail = NULL;
+        }        
+        free(oldest->command);
+        free(oldest);
+    }
+    history_entry_t *new_entry = malloc(sizeof(history_entry_t));
+    if (new_entry == NULL) {
+        return; 
+    }
+    new_entry->command = strdup(command);
+    if (new_entry->command == NULL) {
+        free(new_entry);
+        return; 
+    }
+    
+    new_entry->prev = history_tail;
+    new_entry->next = NULL;
+    if (history_tail != NULL) {
+        history_tail->next = new_entry;
+    } else {
+        history_head = new_entry;
+    }
+    history_tail = new_entry;
+    
+    history_memory_used += command_len;
+    history_count++;
+    history_current = NULL;
+}
+
+static void free_history(void) {
+    history_entry_t *current = history_head;
+    while (current != NULL) {
+        history_entry_t *next = current->next;
+        free(current->command);
+        free(current);
+        current = next;
+    }
+    history_head = NULL;
+    history_tail = NULL;
+    history_current = NULL;
+    history_memory_used = 0;
+    history_count = 0;
+}
+static void sigint_handler(int sig) {
+    if (termios_initialized) {
+        tcsetattr(STDIN_FILENO, TCSANOW, &original_termios_global);
+    }
+    if (sig == SIGINT) {
+        printf("\nReceived interrupt signal\n");
+    } else if (sig == SIGTERM) {
+        printf("\nReceived termination signal\n");
+    }
+    exit(1);
+}
 
 static void set_terminal_mode(struct termios *original) {
     struct termios new_termios;
     tcgetattr(STDIN_FILENO, original);
     new_termios = *original;
-    new_termios.c_lflag &= ~(ICANON | ECHO); 
+    new_termios.c_lflag &= ~(ICANON | ECHO | ISIG); 
     new_termios.c_cc[VMIN] = 1;
     new_termios.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
@@ -50,6 +128,7 @@ static void set_terminal_mode(struct termios *original) {
 static void reset_terminal_mode(struct termios *original) {
     tcsetattr(STDIN_FILENO, TCSANOW, original);
 }
+
 static char* read_line(void) {
     static char buffer[MAX_INPUT_LENGTH];
     int pos = 0;
@@ -64,11 +143,10 @@ static char* read_line(void) {
         if (c == NEWLINE_CHAR) {
             buffer[pos] = NULL_TERMINATOR;
             printf("\n");
-            if (pos > 0 && history_count < HISTORY_SIZE) {
-                strncpy(history[history_count], buffer, MAX_INPUT_LENGTH);
-                history_count++;
+            if (pos > 0) {
+				add_to_history(buffer);
             }
-            history_current = history_count;
+            history_current = NULL;
             reset_terminal_mode(&original_termios);
             return buffer;
         }
@@ -93,38 +171,43 @@ static char* read_line(void) {
             if (c == KEY_LEFT_BRACKET) { 
                 c = getchar();
                 if (c == KEY_UP_ARROW) {
-                    if (history_current > 0) {
-                        history_current--;
+                    if (history_current == NULL) {
+                        history_current = history_tail;
+                    } else if (history_current->prev != NULL) {
+                        history_current = history_current->prev;
+                    }
+                    if (history_current != NULL) {
                         for (int i = 0; i < pos; i++) {
                             printf("\b \b");
-                        }                       
-                        strncpy(buffer, history[history_current], MAX_INPUT_LENGTH);
+                        }
+                        strncpy(buffer, history_current->command, MAX_INPUT_LENGTH);
                         pos = strlen(buffer);
-                        cursor_pos = pos;  
+                        cursor_pos = pos;
                         printf("%s", buffer);
                         fflush(stdout);
                     }
                 }
                 else if (c == KEY_DOWN_ARROW) {
-                    if (history_current < history_count - 1) {
-                        history_current++;
+                    if (history_current != NULL) {
+                        if (history_current->next != NULL) {
+                            history_current = history_current->next;
+                        } else {
+                            history_current = NULL;
+                        }
                         for (int i = 0; i < pos; i++) {
                             printf("\b \b");
                         }
-                        strncpy(buffer, history[history_current], MAX_INPUT_LENGTH);
-                        pos = strlen(buffer);
-                        cursor_pos = pos;  
-                        printf("%s", buffer);
+                        if (history_current != NULL) {
+                            strncpy(buffer, history_current->command, MAX_INPUT_LENGTH);
+                            pos = strlen(buffer);
+                            cursor_pos = pos;
+                            printf("%s", buffer);
+                        } else {
+                            buffer[0] = NULL_TERMINATOR;
+                            pos = 0;
+                            cursor_pos = 0;
+                        }
                         fflush(stdout);
-                    }
-                    else if (history_current == history_count - 1) {
-                        history_current++;
-                        for (int i = 0; i < pos; i++) {
-                            printf("\b \b");
-                        }
-                        buffer[0] = NULL_TERMINATOR;
-                        pos = 0;
-                        cursor_pos = 0;  
                     }
                 }
                 else if (c == KEY_LEFT_ARROW) {
