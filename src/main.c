8c4635486b0e263ab3d9406468e59b7194b465f0
mihasik258 <miha.kuritsin@gmail.com>
2025-09-12 04:06:41 +0300

    finaly history xD

--- src/main.c
+++ src/main.c
@@ -133,7 +133,6 @@ static void set_terminal_mode(struct termios *original) {
 static void reset_terminal_mode(struct termios *original) {
     tcsetattr(STDIN_FILENO, TCSANOW, original);
 }
-
 static char* read_line(void) {
     static char buffer[MAX_INPUT_LENGTH];
     int pos = 0;
@@ -153,14 +152,13 @@ static char* read_line(void) {
         printf("%s", buffer);
     }
     fflush(stdout);
-    int count;
     while (1) {
         c = getchar();
         if (c == NEWLINE_CHAR) {
             buffer[pos] = NULL_TERMINATOR;
             printf("\n");
             if (pos > 0) {
-				add_to_history(buffer);
+                add_to_history(buffer);
             }
             history_current = NULL;
             unfinished_input[0] = NULL_TERMINATOR;
@@ -170,7 +168,7 @@ static char* read_line(void) {
         }
         else if (c == KEY_BACKSPACE || c == KEY_DELETE) {
             if (pos > 0 && cursor_pos > 0) {
-                memmove(&buffer[cursor_pos - 1], &buffer[cursor_pos], pos - cursor_pos);
+                memmove(&buffer[cursor_pos - 1], &buffer[cursor_pos], pos - cursor_pos + 1);
                 pos--;
                 cursor_pos--;
                 printf("\b");
@@ -184,98 +182,57 @@ static char* read_line(void) {
                 fflush(stdout);
             }
         }
-        else if (c >= ASCII_SPACE && c < ASCII_DEL && pos < MAX_INPUT_LENGTH - 1) {
-            if (cursor_pos < pos) {
-                memmove(&buffer[cursor_pos + 1], &buffer[cursor_pos], pos - cursor_pos);
-            }
-            buffer[cursor_pos] = c;
-            pos++;
-            cursor_pos++;
-            if (cursor_pos == pos) {
-                printf("%c", c);
-            } else {
-                for (int i = cursor_pos - 1; i < pos; i++) {
-                    printf("%c", buffer[i]);
-                }
-                for (int i = 0; i < pos - cursor_pos; i++) {
-                    printf("\b");
-                }
-            }
-            fflush(stdout);
-            if (has_unfinished_input) {
-               has_unfinished_input = false;
-               unfinished_input[0] = NULL_TERMINATOR;
-            }
-        }
-        else if (c == KEY_CTRL_C) {
-            printf("\n");
-            reset_terminal_mode(&original_termios);
-            return NULL;
-        }
-        else if (c == KEY_CTRL_D) {
-            if (pos == 0) {
-                printf("\n");
-                reset_terminal_mode(&original_termios);
-                return NULL;
-            }
-        }
         else if (c == KEY_ESCAPE) {
             c = getchar();
             if (c == KEY_LEFT_BRACKET) {
                 c = getchar();
                 if (c == KEY_UP_ARROW) {
-                	if (cursor_pos > 0 && count == 0) {
+                    if (history_current == NULL && pos > 0) {
                         strncpy(unfinished_input, buffer, MAX_INPUT_LENGTH);
                         has_unfinished_input = true;
-                        count = 1;
                     }
-                    if (history_current == NULL) {
-                    	history_current = history_tail;
-                    } else if (history_current->prev != NULL) {
-                        history_current = history_current->prev;
+                    if (history_tail != NULL) {
+                        if (history_current == NULL) {
+                            history_current = history_tail;
+                        }
+                        else if (history_current->prev != NULL) {
+                            history_current = history_current->prev;
+                        }
+                        for (int i = 0; i < pos; i++) {
+                            printf("\b \b");
+                        }
+                        strncpy(buffer, history_current->command, MAX_INPUT_LENGTH);
+                        pos = strlen(buffer);
+                        cursor_pos = pos;
+                        printf("%s", buffer);
                     }
-                    if (history_current != NULL) {
-						for (int i = 0; i < pos; i++) {
-            			printf("\b \b");
-	        			}
-	                    strncpy(buffer, history_current->command, MAX_INPUT_LENGTH);
-	                    pos = strlen(buffer);
-	                    cursor_pos = pos;
-	                    printf("%s", buffer);
-	                    fflush(stdout);
-	                }
+                    fflush(stdout);
                 }
                 else if (c == KEY_DOWN_ARROW) {
                     if (history_current != NULL) {
                         if (history_current->next != NULL) {
                             history_current = history_current->next;
-                        } else {
-                        	history_current = NULL;
                         }
-	        			for (int i = 0; i < pos; i++) {
-            				printf("\b \b");
-       					}
+                        else {
+                            history_current = NULL;
+                        }
+                        for (int i = 0; i < pos; i++) {
+                            printf("\b \b");
+                        }
                         if (history_current != NULL) {
                             strncpy(buffer, history_current->command, MAX_INPUT_LENGTH);
-                            pos = strlen(buffer);
-                            cursor_pos = pos;
-                            printf("%s", buffer);
-                        } else {
-                        	if (has_unfinished_input && count) {
-                        		strncpy(buffer, unfinished_input, MAX_INPUT_LENGTH);
-                        	    pos = strlen(buffer);
-                        	    cursor_pos = pos;
-                        	    printf("%s", buffer);
-                        	    has_unfinished_input = false;
-                        	    count = 0;
-                        	} else{
-                            	buffer[0] = NULL_TERMINATOR;
-                            	pos = 0;
-                            	cursor_pos = 0;
-                            }
                         }
-                        fflush(stdout);
+                        else if (has_unfinished_input) {
+                            strncpy(buffer, unfinished_input, MAX_INPUT_LENGTH);
+                        }
+                        else {
+                            buffer[0] = NULL_TERMINATOR;
+                        }
+                        pos = strlen(buffer);
+                        cursor_pos = pos;
+                        printf("%s", buffer);
                     }
+                    fflush(stdout);
                 }
                 else if (c == KEY_LEFT_ARROW) {
                     if (cursor_pos > 0) {
@@ -291,6 +248,43 @@ static char* read_line(void) {
                         fflush(stdout);
                     }
                 }
+            }
+        }
+        else if (c >= ASCII_SPACE && c < ASCII_DEL && pos < MAX_INPUT_LENGTH - 1) {
+            if (cursor_pos < pos) {
+                memmove(&buffer[cursor_pos + 1], &buffer[cursor_pos], pos - cursor_pos);
+            }
+            buffer[cursor_pos] = c;
+            pos++;
+            cursor_pos++;
+            buffer[pos] = NULL_TERMINATOR;
+            if (cursor_pos == pos) {
+                printf("%c", c);
+            }
+            else {
+                for (int i = cursor_pos - 1; i < pos; i++) {
+                    printf("%c", buffer[i]);
+                }
+                for (int i = 0; i < pos - cursor_pos; i++) {
+                    printf("\b");
+                }
+            }
+            fflush(stdout);
+            if (has_unfinished_input) {
+                has_unfinished_input = false;
+                unfinished_input[0] = NULL_TERMINATOR;
+            }
+        }
+        else if (c == KEY_CTRL_C) {
+            printf("\n");
+            reset_terminal_mode(&original_termios);
+            return NULL;
+        }
+        else if (c == KEY_CTRL_D) {
+            if (pos == 0) {
+                printf("\n");
+                reset_terminal_mode(&original_termios);
+                return NULL;
             }
         }
     }
